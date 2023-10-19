3c03e0f224d4cb3736aaa1d6c840bedaba21b87f
felipehcb <102963067+xelipe12@users.noreply.github.com>
2023-10-19 09:14:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play('');
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0;
+
+while(contador <9){
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#som_tecla_clap');
+    };
+
+    contador = contador + 1;
+
+}
+
+
+
+listaDeTeclas[1].onclick = tocaSomClap
+listaDeTeclas[2].onclick = tocaSomTim
+listaDeTeclas[3].onclick = tocaSomPuff
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom ();
